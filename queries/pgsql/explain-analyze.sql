ec19cb4fc3330305f77b0ab7314106d34edc2d25
Mark Wong <markwkm@gmail.com>
2026-07-27 06:56:44 -0700

    pgsql: collect more data with auto_explain

--- queries/pgsql/explain-analyze.sql
+++ queries/pgsql/explain-analyze.sql
@@ -1,6 +1,8 @@
 load 'auto_explain';
 set auto_explain.log_min_duration to 0;
 set auto_explain.log_analyze to on;
+set auto_explain.log_buffers to on;
 set auto_explain.log_wal to on;
 set auto_explain.log_timing to on;
 set auto_explain.log_level to notice;
+set auto_explain.log_verbose to on;
